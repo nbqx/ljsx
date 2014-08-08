f41d6ae3170c5232da225e54077d21f7e0990ddd
n.b.q.x <nbqxnbq@gmail.com>
2014-08-08 20:04:02 +0900

    add -x option

--- README.md
+++ README.md
@@ -7,7 +7,18 @@ ljsx = markdown + extendscript
 ## Usage
 
     $ npm install -g https://github.com/nbqx/ljsx.git
+
+then
+
     $ ljsx test.ljsx -o output.jsx
+
+output extendscript
+
+or
+
+    $ ljsx -x test.ljsx
+
+execute extendscript via [fakestk](https://github.com/nbqx/fakestk)
 
 see example.ljsx
 
--- cmd.js
+++ cmd.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 var fs = require('fs'),
-    argv = require('yargs').argv;
+    argv = require('yargs')
+      .boolean('x')
+      .argv,
+    es = require('event-stream'),
+    fakestk = require('fakestk');
 var ljsx = require('./');
 
 var opts = { gfm:true };
@@ -10,9 +14,22 @@ if(argv.o || argv.out) out = argv.o || argv.out;
 
 if(argv._.length!==0){
   var tgt = argv._[0];
-  if(out===undefined){
-    ljsx(tgt,opts).pipe(process.stdout);
+
+  if(argv.x){
+    var input = '';
+    ljsx(tgt,opts).pipe(es.through(function(data){
+      input += data.toString();
+    },function(){
+      fakestk.run(input,function(err,res){
+        if(err) return console.log(err);
+        if(res!=='') console.log(res);
+      });
+    }));
   }else{
-    ljsx(tgt,opts).pipe(fs.createWriteStream(out));
+    if(out===undefined){
+      ljsx(tgt,opts).pipe(process.stdout);
+    }else{
+      ljsx(tgt,opts).pipe(fs.createWriteStream(out));
+    }
   }
 }
