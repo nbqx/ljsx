d80486d247639ac6383b381d0670ff8db28be1fe
n.b.q.x <nbqxnbq@gmail.com>
2014-08-29 11:42:04 +0900

    accept buffer

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var fs = require('fs');
 var marked = require('marked'),
     es = require('event-stream');
 
-module.exports = function(file,markedOpts){
-  var cont = fs.readFileSync(file)+'';
+var cont;
+
+module.exports = function(src,markedOpts){
+  if(Buffer.isBuffer(src)){
+    cont = src + '';
+  }else{
+    cont = fs.readFileSync(src)+'';
+  }
   var lex = marked.lexer(cont,markedOpts);
   return es.readArray(lex).pipe(es.through(function(data){
     if(data.type==='code'){
